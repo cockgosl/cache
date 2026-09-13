6a64884160c7d579ec86c6d61ac5c61f6f14d054
cockgosl <Igra.nikolaev@gmail.com>
2026-09-13 20:13:56 +0300

    the start of the cash_project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+using namespace std;
+
+size_t cash_func(size_t s, istream& is) {
+    size_t hits = 0;
+    list<int> pages;
+    unordered_map<int, list<int>::iterator> table;
+
+
+    return hits;
+}
+
+int main() {
+    return 0;
+}
